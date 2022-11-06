6f0abba87cba042887a721b74e16286088e07070
Christopher Warren <chrisalmith@gmail.com>
2022-11-06 00:24:51 -0400

    cleanup

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -80,7 +80,7 @@ export const Navbar = () => {
           <ul className="flex">
             {navLinks.map((route: NavPath) => {
               const isCurrentPage = router.pathname.includes(route.href);
-              console.log(router.pathname.includes(route.href));
+
               return (
                 <li
                   key={route.pathname}
