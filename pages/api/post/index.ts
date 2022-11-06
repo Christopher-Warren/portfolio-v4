1e15ab7ce87447ece7f9d8d4f4a76410f6f0a93a
Christopher Warren <chrisalmith@gmail.com>
2022-11-05 23:40:09 -0400

    sanitize slug uri

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -4,19 +4,41 @@ import prisma from "../../../lib/prisma";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
+interface ExtendedNextRequest extends NextApiRequest {
+  body: {
+    title: string;
+    content: string;
+  };
+}
+
 export default async function handle(
-  req: NextApiRequest,
+  req: ExtendedNextRequest,
   res: NextApiResponse<any>
 ) {
   const { title, content } = req.body;
 
   const session = await getSession({ req });
+
+  const isAdmin = session?.user?.email === "chrisalmith@gmail.com";
+
+  const sanitizedUri = title
+    .replace(/[^a-zA-Z 0-9]/g, "")
+    .trimEnd()
+    .trimStart()
+    .replaceAll(" ", "-");
+
+  if (!session || !isAdmin) {
+    res.json({ unauthorized: "You must be an admin to create posts" });
+  }
+
   const result = await prisma.post.create({
     data: {
       title,
       content,
+      slug: sanitizedUri,
       author: { connect: { email: session?.user?.email || undefined } },
     },
   });
+
   res.json(result);
 }
